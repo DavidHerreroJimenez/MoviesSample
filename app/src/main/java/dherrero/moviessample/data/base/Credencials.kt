f10172fa988a9ce30bf6db6c6506109278e59f38
David Herrero Jiménez <daheji@gmail.com>
2018-06-25 08:04:51 +0200

    credentials deleted

--- app/src/main/java/dherrero/moviessample/data/base/Credencials.kt
+++ app/src/main/java/dherrero/moviessample/data/base/Credencials.kt
@@ -4,8 +4,8 @@ class Credencials{
 
     companion object {
 
-        const val CREDENCIAL_THEMOVIEDB_API_V3 = "93aea0c77bc168d8bbce3918cefefa45"
-        //    https://api.themoviedb.org/3/list/80531?api_key=ca7986f8cb4b92c2549001ad4c176934&language=es-ES
+        const val CREDENCIAL_THEMOVIEDB_API_V3 = "yourcredentials"
+
 
     }
 }
